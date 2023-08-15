0c9b641357375b768d53d7e1027c9dd695d1f879
Giacomo Fregara <jack.freg@gmail.com>
2023-08-15 11:53:21 +0200

    BAS algorithm concluded

--- BAS.m
+++ BAS.m
@@ -15,24 +15,25 @@ close all;
 % Parameters
 n_dims = 3;     % search space dimension
 d = 3;          % antennae's sensing length
-n = 100;        % max number of iterations
+d0 = 0.001;     %% Testing % Constant
 eta = 0.95;     % step factor
 delta = 10;     % search step size
+n = 100;        % max number of iterations
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Testing
 n_dims = 2;
 % d = 2;
-% delta = 0.5;
+delta = 0.8;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 
 % Beetle position and orientation randomly initialized
-dim = [n_dims, 1];
-x = rand(dim);      % random position of beetle
+sz = [n_dims, 1];
+x = 2 * rand(sz);   % random position of beetle
 x = x / norm(x);
 
 % Data history for visualization
-x_history = zeros(dim) * ones(1, n);
+x_history = zeros(sz) * ones(1, n);
 y_history = zeros(n, 1);
 
 % First cost function value
@@ -47,13 +48,12 @@ for i=1:n
     y_history(i) = y;
 
     % Random search direction
-    b = rand(dim);      
+    b = rand(sz);      
     b = b / norm(b);
 
     % Left and right antennae's position update
-    x_r = x + d * b;
+    x_r = x + d * b;    
     x_l = x - d * b;
-    
     % Update beetle's position
     x = x + delta * sign(cost_fun(x_l) - cost_fun(x_r)) * b;
     y = cost_fun(x);
@@ -65,8 +65,9 @@ for i=1:n
     
     % Update d and delta
     % Comment next two lines to set d and delta as constants
-    % d = d * eta + 0.01;
+    d = d * eta + d0;
     delta = delta * eta;
+    
 end
 
 % Visualization
@@ -77,21 +78,38 @@ if doPlot == true
         y_star = cost_fun(x_star);
 
         % Plot
+        % Plot beetle path, global minimum in 2D, local minimum found and
+        % final position of the beetle
         figure;
         hold on;
         plot(x_history(1,:), x_history(2,:), '-o');
         plot(x_star(1), x_star(2), 'r*');
+        plot(x_best(1), x_best(2), 'b+');
         plot(x_history(1,end), x_history(2,end), 'kx');
         hold off;
         xlabel('x_1');
         ylabel('x_2');
         title('Beetle path');
-        legend('Beetle path', 'Global minimum', 'Final position')
+        legend('Beetle path', 'Global minimum', 'Local minimum', 'Final position')
+        grid on;
+
+        % Plot cost function
+        figure;
+        hold on;
+        plot(1:n, y_history, '-o');
+        plot([1 n], [y_best y_best], 'Color','k', 'LineStyle',':');
+        plot([1 n], [y_star y_star], "Color","g", "LineStyle","--");
+        hold off;
+        xlabel('Iterations');
+        ylabel('Cost function');
+        title('Cost function exploration');
+        legend('Cost function', 'Local minimum', 'Global minimum');
         grid on;
     elseif n_dims == 3
+        % Plot for 3-dimensional cost function
         figure;
-        plot3(x_history(1,:), x_history(2,:), x_history(3,:), 'Color', 'r', LineStyle='-.');
         hold on;
+        plot3(x_history(1,:), x_history(2,:), x_history(3,:), 'Color', 'r', LineStyle='-.');
         plot3(x_best(1), x_best(2), x_best(3), 'Color', 'black', 'Marker','x');
         plot3(x_history(1,end), x_history(2,end), x_history(3, end), 'Color','green', 'Marker', '*');
         hold off;
