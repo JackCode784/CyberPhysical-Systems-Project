41c748d722da475bcfc54f246928ed6ccf17b4cd
Giacomo Fregara <jack.freg@gmail.com>
2023-08-19 17:22:30 +0200

    added simulation.m to script simulations of boat subsystem

---
+++ Simulation.m
@@ -0,0 +1,6 @@
+%% Simulation Script
+% This script serves as a starting point to simulate the simulink model of
+% the boat and correctly assign PID parameters according to the BAS
+% algorithm.
+% 
+
