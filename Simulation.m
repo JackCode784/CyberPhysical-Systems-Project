9811fbc3a69361c4bf254052ddd333d7b8d99547
FrancescaV <s4775728@studenti.unige.it>
2023-08-20 11:48:50 +0200

    edit bas in system.m and simulation.m

--- Simulation.m
+++ Simulation.m
@@ -3,4 +3,9 @@
 % the boat and correctly assign PID parameters according to the BAS
 % algorithm.
 % 
-
+% n= 100;
+% y = zeros(1,n);
+% for i=1:n
+%     Out = sim('System.slx')
+%     y(i) = Out;
+% end
