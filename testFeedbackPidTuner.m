6d7c53a00614dbbc0890b0bdab1c7c03006a932a
Giacomo Fregara <jack.freg@gmail.com>
2023-08-22 17:33:50 +0200

    updated test and system

--- testFeedbackPidTuner.m
+++ testFeedbackPidTuner.m
@@ -8,19 +8,26 @@ Gf = feedback(G, 1);
 % Closed loop step response
 step(Gf);
 % ylim([0 2]);
+grid on;
 
 % Root locus open loop system
 figure;
 rlocus(G);
+grid on;
 
 % Use PID
 R = pidtune(G, 'PID');
 S = feedback(G*R, 1);
 
+% Set PID params
+k = [R.Kp, R.Ki, R.Kd];
+
 % Closed loop step response with PID
 figure;
 step(S);
+grid on;
 
 % Complete system root locus
 figure;
 rlocus(S);
+grid on;
